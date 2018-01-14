2333b0a7505fbaa060ed65dd75728610b2b79aff
dbilderback <dbilderback@gmail.com>
2018-01-14 17:50:12 -0500

    test

--- getresponse.js
+++ getresponse.js
@@ -3,7 +3,7 @@ function getResponse() {
   var APIKey = "7cfbab76-7af1-4ce7-b579-d1662046422d";
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
   // Here we are building the URL we need to query the database
-  var queryURL = "http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey="+ APIKey;
+  var queryURL = "developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey="+ APIKey;
   //Ajax call to get the response object
   $.ajax({
     url: proxyurl+queryURL,
@@ -14,8 +14,11 @@ function getResponse() {
     //var station = groupDestinations(response, 'DESTINATIONS');
     var stations = groupDestinations(response, 'DESTINATION');
     //console.log(stations);
-    var arrivals = getStationArrivals(response, 'FIVE POINTS STATION', 'STATION');
-    //console.log(arrivals);
+    var arrivals = getArrivals(response, 'FIVE POINTS STATION', 'STATION', 'NEXT_ARR', 'TRAIN_ID');
+    console.log(arrivals);
+    var trainStops = arrivals.returnTrainStops(response, 'TRAIN_ID');
+    console.log(trainStops);
+    //console.log(response.hasValue("Airport", 'DESTINATION'));
     //fail promise to deal with unknown exceptions
   }).fail(function (jqXHR, textStatus) {
     alert('The request for your subject failed please try another button');
@@ -31,17 +34,43 @@ function groupDestinations(array, property) {
     return hash;
 }
 
-function getStationArrivals(currentTrains, station, property) {
+function getArrivals(currentTrains, station, property, returnString1, returnString2) {
   var hash = {};
+  arrivals = [];
   for (var i =0; i < currentTrains.length; i++) {
     if (!hash[currentTrains[i][property]]) hash[currentTrains[i][property]] = [];
         //console.log(currentTrains[i][property].toString());
         if (currentTrains[i][property].toString() == station) {
-          console.log(currentTrains[i]);
+          var nextArrvial = [] 
+          nextArrvial.push(currentTrains[i][returnString1]);
+          nextArrvial.push(currentTrains[i][returnString2]); 
+          arrivals.push(nextArrvial);
           //hash[currentTrains[i][property]].push(currentTrains[i]);  
         }
          
   }
-  return hash;
+  return arrivals;
+}
 
+function getTrainStops(arrivals, response) {
+  for (var i =0; i < arrivals.length; i++) {
+
+  }
+}
+Array.prototype.returnTrainStops = function(response, trainID) {
+    var trainStops = []; 
+    var i = this.length;
+    while (i--) {
+      var x = response.length;
+        while(x--) {
+          //if (this[i][property] == filter) {
+            console.log(this[i]);
+        if (response[x][trainID] == this[i][1]) {
+          console.log(this[i][1]);
+          trainStops.push(response[x]);   // Found it
+        //}
+        }  
+      }
+    }
+    return trainStops;
 }
